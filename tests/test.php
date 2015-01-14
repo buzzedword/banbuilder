6fb9de46124ba397547470966b2f0881c6958350
snipe <snipe@snipe.net>
2015-01-14 12:00:49 -0800

    Updated test

--- tests/test.php
+++ tests/test.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/../vendor/autoload.php'; // Autoload files using Composer autoload
 
-use BanBuilder\CensorWords;
+use Snipe\BanBuilder\CensorWords;
 
 echo CensorWords::censorString();
